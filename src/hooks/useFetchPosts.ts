0aa38dc4239980b7d9d3e2584c821cc3fced4d23
Julieta <julietaben16@gmail.com>
2025-04-12 12:49:01 -0500

    Revert "Fixed myposts bug"
    This reverts commit 07dcfbfdad8460b335b0b08f6c6c38146344cc11.

--- src/hooks/useFetchPosts.ts
+++ src/hooks/useFetchPosts.ts
@@ -2,13 +2,11 @@ import { useState, useEffect, useContext } from "react";
 import supabase from "../supabase/supabase";
 
 import { AuthContext } from "../contexts/AuthContext";
-import { Pet, Report } from "../../types";
+import { Pet } from "../../types";
 
 export const useFetchPosts = () => {
   const user = useContext(AuthContext);
-  const [pets, setPets] = useState<Pet[]>([]);
-  const [reports, setReports] = useState<Report[]>([]);
-  const posts = [...pets, ...reports];
+  const [posts, setPosts] = useState<Pet[]>([]);
 
   const [loading, setLoading] = useState(false);
 
@@ -29,39 +27,13 @@ export const useFetchPosts = () => {
         setLoading(false);
         return;
       }
-      const activePets = data.filter(
-        (pet: Pet) => !pet.adopted && !pet.deleted
+      const activePosts = data.filter(
+        (post: Pet) => !post.adopted && !post.deleted
       );
-      setPets(activePets);
+      setPosts(activePosts);
       setLoading(false);
     };
     fetchPets();
-  }, [user]);
-
-  useEffect(() => {
-    if (!user?.id) {
-      return;
-    }
-
-    const fetchReports = async () => {
-      setLoading(true);
-      const { data, error } = await supabase
-        .from("reports")
-        .select("*")
-        .eq("user", user.id);
-
-      if (error || !data) {
-        console.log(error || "No data received");
-        setLoading(false);
-        return;
-      }
-      const activeReports = data.filter(
-        (report: Report) => !report.found && !report.deleted
-      );
-      setReports(activeReports);
-      setLoading(false);
-    };
-    fetchReports();
   }, [user]);
 
   return { posts, loading };
