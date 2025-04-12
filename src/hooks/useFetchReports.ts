b79621e68a1539903de64164009540a29ad320dd
Julieta <julietaben16@gmail.com>
2025-04-12 11:51:48 -0500

    fix

--- src/hooks/useFetchReports.ts
+++ src/hooks/useFetchReports.ts
@@ -1,7 +1,6 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect } from "react";
 import supabase from "../supabase/supabase";
 
-import { AuthContext } from "../contexts/AuthContext";
 import { Report } from "../../types";
 
 export const useFetchReports = () => {
