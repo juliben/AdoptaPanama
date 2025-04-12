e4655ee5c3f189bd73a9401e86b0b27c31c714a0
Julieta <julietaben16@gmail.com>
2025-04-12 12:19:12 -0500

    heart takes you to homepage now

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -1,11 +1,13 @@
 import { LuMenu } from "react-icons/lu";
 import { motion } from "motion/react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   onClick: () => void;
 };
 
 export const HamburgerMenu = ({ onClick }: Props) => {
+  const navigate = useNavigate();
   return (
     <div className="sm:hidden flex justify-between mx-5 mt-4 hover:cursor-pointer ">
       <motion.img
@@ -16,6 +18,7 @@ export const HamburgerMenu = ({ onClick }: Props) => {
           rotate: 360,
           transition: { duration: 1, ease: "easeOut" },
         }}
+        onClick={() => navigate("/")}
       />
       <motion.button
         whileTap={{ scale: 0.93 }}
