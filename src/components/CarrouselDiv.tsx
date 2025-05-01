a92eaaecdea8e9bd1c560f9622600b2b69b68658
Julieta <julietaben16@gmail.com>
2025-05-01 14:57:25 -0500

    fix responsiveness

--- src/components/CarrouselDiv.tsx
+++ src/components/CarrouselDiv.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { motion } from "motion/react";
 import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import { PetCardTwoAnimals } from "./PetCardTwoAnimals";
 
 export const CarrouselDiv = () => {
   const user = useContext(AuthContext);
@@ -45,26 +46,53 @@ export const CarrouselDiv = () => {
     return <PetCardOnlyOne animal={animals[0]} />;
   }
 
-  return (
-    <div className="relative h-auto min-h-[530px] lg:min-h-[700px] bg-light-gray flex flex-col rounded-3xl py-8 px-4 md:px-6">
+  if (animals.length === 2) {
+    return   <div className="relative h-auto min-h-[530px] lg:min-h-[700px] bg-light-gray flex flex-col rounded-3xl py-8 px-4 md:px-6">
+    <div className="w-full max-w-screen-xl mx-auto">
       <h2 className="font-semibold mb-5 text-2xl sm:text-3xl pl-1 sm:pl-2">
         Animales en adopción
       </h2>
-  
-      <div className="w-full overflow-x-auto">
-        <div className="flex flex-nowrap gap-4 px-1 sm:px-3">
+
+      <div className="w-full overflow-x-auto sm:overflow-x-hidden ml-20">
+        <div className="flex flex-nowrap gap-4 sm:gap-0 px-1 sm:px-3">
           {shownAnimals.map((animal, index) => (
-            <PetCard key={index} animal={animal} index={index} />
+            <PetCardTwoAnimals key={index} animal={animal} index={index} />
           ))}
         </div>
       </div>
-  
+
       <button
         onClick={() => navigate("/listings")}
         className="mt-6 lg:mt-10 self-center rounded-full px-6 py-2 border border-gray-400 hover:bg-gray-100 transition"
       >
         Ver todos
       </button>
+    </div>
+  </div>;
+  }
+
+  return (
+    <div className="relative h-auto min-h-[530px] lg:min-h-[700px] bg-light-gray flex flex-col rounded-3xl py-8 px-4 md:px-6">
+      <div className="w-full max-w-screen-xl mx-auto">
+        <h2 className="font-semibold mb-5 text-2xl sm:text-3xl pl-1 sm:pl-2">
+          Animales en adopción
+        </h2>
+  
+        <div className="w-full overflow-x-auto">
+          <div className="flex flex-nowrap gap-4 px-1 sm:px-3">
+            {shownAnimals.map((animal, index) => (
+              <PetCard key={index} animal={animal} index={index} />
+            ))}
+          </div>
+        </div>
+  
+        <button
+          onClick={() => navigate("/listings")}
+          className="mt-6 lg:mt-10 self-center rounded-full px-6 py-2 border border-gray-400 hover:bg-gray-100 transition"
+        >
+          Ver todos
+        </button>
+      </div>
     </div>
   );
   
---
+++ src/components/PetCardTwoAnimals.tsx
@@ -0,0 +1,78 @@
+import { motion } from "motion/react";
+import { Pet } from "../../types";
+import { useNavigate } from "react-router-dom";
+
+type Props = {
+  animal: Partial<Pet>;
+  index: number;
+};
+
+export const PetCardTwoAnimals = ({ animal, index }: Props) => {
+  const navigate = useNavigate();
+  return (
+    <div className="flex flex-col lg:ml-25">
+      {index === 1 && (
+        <>
+          <p className="text-dark-primary font-semibold  relative top-[-1%] lg:top-[0%] left-[10%] sm:left-[13%]">
+            {animal.name}, {animal.sex}
+          </p>
+          <img
+            src="/flecha-2.png"
+            alt="flecha"
+            className="w-[25%] h-[10%] rotate-20 scale-y-[-1] relative left-[60%] sm:left-[30%] "
+          />
+        </>
+      )}
+      <div
+        className="relative h-[60%] w-[65%]  lg:h-[75%] lg:w-[300px]  overflow-hidden mr-5 min-w-48 min-h-65 bg-white rounded-3xl flex flex-col justify-center text-center"
+        style={{
+          transform: `${index === 1 ? "translateY(4%)" : ""}`,
+        }}
+      >
+        {animal && animal.images ? (
+          <img
+            src={animal.images[0]}
+            alt={animal.name}
+            className="w-full object-cover h-full"
+          />
+        ) : (
+          <p>¡No hay ningún animalito en adopción ahora mismo!</p>
+        )}
+        {animal && (
+          <motion.button
+            onClick={() => navigate(`/pet-details/${animal.id}`)}
+            whileTap={{ scale: 0.9 }}
+            className="absolute text-center bg-accent-light rounded-full w-[90%] self-center py-3 bottom-2 shadow cursor-pointer"
+          >
+            Contactar
+          </motion.button>
+        )}
+      </div>
+
+      {index === 0 && (
+        <>
+          <img
+            src="/flecha-3.png"
+            alt="flecha"
+            className=" w-1/4 h-1/8 rotate-180 self-center relative left-[10%] sm:-left-[17%] mt-1.5 "
+          />
+          <p className="text-dark-primary font-semibold mt-1 pl-3 lg:pl-20">
+            {animal.name}, {animal.sex}
+          </p>
+        </>
+      )}
+      {index === 2 && (
+        <>
+          <img
+            src="/flecha-1.png"
+            alt="flecha"
+            className="w-1/4 h-1/8 rotate-0 self-center relative left-[10%] mt-1.5 "
+          />
+          <p className="text-dark-primary font-semibold mt-1 pl-10 lg:pl-40">
+            {animal.name}, {animal.sex}
+          </p>
+        </>
+      )}
+    </div>
+  );
+};
