0113dbbe5082365e45fddfe6f5d28225ab996aeb
Julieta <julietaben16@gmail.com>
2025-07-03 09:56:49 -0500

    add guard

--- src/components/PetContainer.tsx
+++ src/components/PetContainer.tsx
@@ -16,11 +16,11 @@ export const PetContainer = ({ pet }: Props) => {
       className="relative h-[250px] sm:h-[320px] sm:w-[250px] rounded-2xl overflow-hidden cursor-pointer shadow"
       onClick={handleNavigate}
     >
-      <img
+   {pet.images &&   <img
         src={pet.images[0]}
         alt={pet.name}
         className="bg-light-gray object-cover w-full h-full"
-      />
+      />}
       <div className="absolute bottom-2 left-1/2 -translate-x-1/2 min-w-2/3 max-w-[90%] truncate text-center bg-accent-light rounded-full px-3 py-1 text-lg">
         {pet.name}, {pet.age}
       </div>
--- src/supabase/supabase.ts
+++ src/supabase/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://ibeddmnfzpvqyxmpuffa.supabase.co";
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImliZWRkbW5menB2cXl4bXB1ZmZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQxMjczNjgsImV4cCI6MjA1OTcwMzM2OH0.JVTDQTOtCo33nH4vt_uC34hzRpuxG9QVLP1eqIO6iKQ'
 const supabase = createClient(supabaseUrl, supabaseKey!);
 
 export default supabase;
